704341a500d3ca71318a4edae3027c00bfe378f2
romio-krvtsky <romik.krivetskiy@gmail.com>
2023-03-02 20:53:10 +0300

    added lab1

---
+++ lab1/script-lab1.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+#directory for analysis
+directory=$1
+echo "The content of directory - $directory ."
+echo "Amount of directories = " 
+ls -d $directory*/  | wc -l && 2>>/dev/null
+ls -d $directory*/ 2>>/dev/null
+echo "Amount of files = " 
+find $directory -maxdepth 1 -type f | wc -l
+find $directory -maxdepth 1 -type f 
+
