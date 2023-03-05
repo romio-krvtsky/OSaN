51f5cc1645d6d61225a2eb2174ee31e83b8cf1bf
romio-krvtsky <romik.krivetskiy@gmail.com>
2023-03-05 14:16:33 +0300

    update output of directories

--- lab1/script-lab1.sh
+++ lab1/script-lab1.sh
@@ -5,7 +5,7 @@
 directory=$1
 echo "The content of directory - $directory ."
 echo "Amount of directories = " 
-ls -d $directory*/  | wc -l && 2>>/dev/null
+ls -d $directory*/ 2>>/dev/null  | wc -l
 ls -d $directory*/ 2>>/dev/null
 echo "Amount of files = " 
 find $directory -maxdepth 1 -type f | wc -l
